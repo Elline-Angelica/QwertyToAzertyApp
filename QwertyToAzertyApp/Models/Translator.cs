5d713d99a7cd79c2a074050b8e69140327dcf882
Elline-Angelica <ellinemedrano1408@hotmail.com>
2024-11-06 17:48:23 +0100

    added capitalise first char of each sentence feature + translate when enter key is pressed

--- QwertyToAzertyApp/Models/Translator.cs
+++ QwertyToAzertyApp/Models/Translator.cs
@@ -1,27 +1,59 @@
-namespace QwertyToAzertyApp.Models;
-using System.Collections.Generic;
+using System.Security.Principal;
 
-public static class Translator
+namespace QwertyToAzertyApp.Models
 {
-    private static readonly Dictionary<char, char> QwertyToAzertyMap = new()
-    {
-        { 'a', 'q' }, { 'A', 'Q' }, { 'q', 'a' }, { 'Q', 'A' },
-        { 'z', 'w' }, { 'Z', 'W' }, { 'w', 'z' }, { 'W', 'Z' },
-        { 'm', ',' }, { 'M', '?' }, { ';', 'm' }, { ':', 'M' },
-        { '<', '.' }, { '>', '/' }, {',', '.'} // Add more mappings as needed
-        // Add mappings for uppercase letters and symbols if needed
-    };
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
 
-    public static string TranslateToAzerty(string input)
+    public static class Translator
     {
-        char[] result = input.ToCharArray();
-        for (int i = 0; i < result.Length; i++)
+        private static readonly Dictionary<char, char> QwertyToAzertyMap = new()
         {
-            if (QwertyToAzertyMap.ContainsKey(result[i]))
+            { 'a', 'q' }, { 'A', 'q' }, { 'q', 'a' }, { 'Q', 'a' },
+            { 'z', 'w' }, { 'Z', 'w' }, { 'w', 'z' }, { 'W', 'z' },
+            { 'm', ',' }, { 'M', '?' }, { ';', 'm' }, { ':', 'M' },
+            { '<', '.' }, { '>', '/' }, { ',', '.' }, { '8', '!' }
+        };
+
+        public static string TranslateToAzerty(string input)
+        {
+            char[] result = input.ToCharArray();
+            for (int i = 0; i < result.Length; i++)
             {
-                result[i] = QwertyToAzertyMap[result[i]];
+                if (QwertyToAzertyMap.ContainsKey(result[i]))
+                {
+                    if (i == 0)
+                    {
+                        result[i] = QwertyToAzertyMap[result[i]];
+                    }
+                    result[i] = QwertyToAzertyMap[result[i]];
+                }
+                
             }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (i != 0)
+                {
+                    result[i] = Convert.ToChar(result[i].ToString().ToLower());
+                }
+            }
+
+            string translatedResult = new string(result);
+            return translatedResult.CapitalizeAfter(new []{ '.', '<' });
         }
-        return new string(result);
+        
+        public static string CapitalizeAfter(this string s, IEnumerable<char> chars)
+        {
+            var charsHash = new HashSet<char>(chars);
+            StringBuilder sb = new StringBuilder(s);
+            for (int i = 0; i < sb.Length - 2; i++)
+            {
+                if (charsHash.Contains(sb[i]) && sb[i + 1] == ' ')
+                    sb[i + 2] = char.ToUpper(sb[i + 2]);
+            }
+            return sb.ToString();
+        }
     }
 }
